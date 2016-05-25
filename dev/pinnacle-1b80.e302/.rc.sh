11c24c30a8d90538236de46d351345cbc58f113b
yoosefi <code@yoosefi.info>
2016-05-24 19:21:42 -0700

    pinnacle-1b80.e302 dont set sharpness by default

--- dev/pinnacle-1b80.e302/.rc.sh
+++ dev/pinnacle-1b80.e302/.rc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VIDEO_DEVICE=($(v4l2-ctl --list-devices | awk '/Pinnacle/{getline; print}'))
-v4l2-ctl -d ${VIDEO_DEVICE} -c sharpness=5,mute=0
+v4l2-ctl -d ${VIDEO_DEVICE} -c mute=0
 
 ALSA_ID=($(cat /proc/asound/cards | grep 'USB 2861 Device'))
