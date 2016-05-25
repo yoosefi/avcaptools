b3523626e1674c9b919d4a23868fa3a9c7f1f963
yoosefi <code@yoosefi.info>
2016-05-24 21:48:16 -0700

    pinnacle-1b80.e302 finetuning, added input selection for svideo, limited selection for signal

--- dev/pinnacle-1b80.e302/.rc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-VIDEO_DEVICE=($(v4l2-ctl --list-devices | awk '/Pinnacle/{getline; print}'))
-v4l2-ctl -d ${VIDEO_DEVICE} -c mute=0
-
-ALSA_ID=($(cat /proc/asound/cards | grep 'USB 2861 Device'))
